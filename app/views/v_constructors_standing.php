ef7a87ee770f462fa31161606677b8a0bde66c77
DrunkinDonut <natanu1@wp.pl>
2021-12-22 00:03:56 +0100

    Add teamLogoName to standing template method

--- app/views/v_constructors_standing.php
+++ app/views/v_constructors_standing.php
@@ -11,24 +11,7 @@
     <div class="app_container">
         <h2 class="h2_with_arrow"><a class="arrow_back_icon" href="<?php echo SITE_PATH; ?>app/results.php"><img src="<?php echo APP_RES; ?>img/arrow_back.svg" alt=""></a>Klasyfikacja konstruktorów</h2>
         <div class="standings">
-            <div class="standing">
-                <div class="standing_place_block">
-                    <div class="standing_place">
-                        <div class="standing_place_number">
-                            1
-                        </div>
-                    </div>
-                    <div class="standing_place_logo" style="background-color: #08A49E;">
-                        <img src="<?php echo APP_RES; ?>img/teams/mercedes_white.svg" alt="">
-                    </div>
-                </div>
-                <div class="standing_name">
-                    Mercedes
-                </div>
-                <div class="standing_points">
-                    573
-                </div>
-            </div>
+        <?php $this->Template->showStanding(1, 'mercedes_white', 'Mercedes', 573); ?>
         </div>
     </div>
 </body>
